9778bc7cea7c2b871416222ea89cdb1823946405
pedro-lisboa <51640519+pedro-lisboa@users.noreply.github.com>
2020-03-01 02:03:47 -0300

    Add files via upload

---
+++ Desafio Enquete/Desafio_Database/Conexao.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace Desafio_DataBase
+{
+    public class Conexao
+    {
+        //string StrConexao = (@"data source=localhost\SQLEXPRESS;Integrated Security=SSPI;Initial Catalog=DB_ENQUETE");
+        string StrConexao = (@"data source=localhost\SQLSERVER;Integrated Security=SSPI;Initial Catalog=DB_ENQUETE");
+        private SqlConnection AbrirBanco()
+        {
+            SqlConnection cn = new SqlConnection(StrConexao);
+            cn.Open();
+            return cn;
+        }
+
+
+        public void ExecutarComando(StringBuilder strQuery)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn = AbrirBanco();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = strQuery.ToString();
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public SqlDataReader RetornoReader(string strQuery)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn = AbrirBanco();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = strQuery.ToString();
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Connection = cn;
+                return cmd.ExecuteReader();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
